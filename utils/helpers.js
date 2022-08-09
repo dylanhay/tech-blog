980780483e125fda0261e54be92afbbf6ac11814
dchay <dylanhay@rogers.com>
2022-08-09 17:45:21 -0400

    url formatting function and test introduced

--- utils/helpers.js
+++ utils/helpers.js
@@ -11,5 +11,15 @@ module.exports = {
     }
 
     return word;
-  }
+  },
+
+  format_url: url => {
+    return url
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
+  },
 };
+
