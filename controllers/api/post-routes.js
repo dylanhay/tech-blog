eff47c9b2bc3b7d51037f91dd588dc73584cc25f
dchay <dylanhay@rogers.com>
2022-08-10 06:16:09 -0700

    routes protected with middleware

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
+const withAuth = require('../../utils/auth');
 
 // get all posts
 router.get("/", (req, res) => {
@@ -84,7 +85,7 @@ router.get("/:id", (req, res) => {
 });
 
 //create a post
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster News!', content: 'Taskmaster has decided to go public', user_id: 1}
   Post.create({
     title: req.body.title,
@@ -99,7 +100,7 @@ router.post("/", (req, res) => {
 });
 
 //update a post
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -124,7 +125,7 @@ router.put("/:id", (req, res) => {
 });
 
 //delete a post
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id,
